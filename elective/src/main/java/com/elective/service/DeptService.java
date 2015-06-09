a534268471c318ff44c230665169a8a18a6f1e75
周勇 <yong.zhou@rrs.com>
2015-06-09 22:54:35 +0800

    增加map转bean

--- elective/src/main/java/com/elective/service/DeptService.java
+++ elective/src/main/java/com/elective/service/DeptService.java
@@ -2,13 +2,19 @@ package com.elective.service;
 
 import com.bstek.bdf2.core.business.IDept;
 import com.bstek.bdf2.core.service.IDeptService;
+import com.dosola.core.common.DosolaUtil;
 import com.dosola.core.dao.interfaces.IMasterDao;
 import com.elective.pojo.Dept;
 import com.elective.pojo.DeptUser;
 import com.elective.pojo.User;
+import org.apache.commons.beanutils.BeanUtils;
+import org.apache.commons.beanutils.BeanUtilsBean;
 import org.apache.commons.lang.StringUtils;
 
 import javax.annotation.Resource;
+import java.beans.IntrospectionException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -34,14 +40,24 @@ public class DeptService implements IDeptService{
             return null;
         }
         StringBuffer sb = new StringBuffer();
-        sb.append(" select d from ");
-        sb.append(Dept.class.getName()+" as d inner join ");
-        sb.append(DeptUser.class.getName()+" as du with d.id=du.deptId inner join ");
-        sb.append(User.class.getName()+" as u with u.id=du.userId");
+        sb.append(" select d.* from ");
+        sb.append(Dept.TABLENAME+" as d join ");
+        sb.append(DeptUser.TABLENAME+" as du on d.id=du.deptId join ");
+        sb.append(User.TABLENAME+" as u on u.id=du.userId");
         sb.append(" where u.username=:username ");
         Map<String,Object> params = new HashMap<String, Object>();
         params.put("username",username);
-        return (List<IDept>)(dao.query(sb.toString(),params));
+        List<Map<String,Object>> list = dao.queryBySql(sb.toString(),params);
+        try {
+            List<IDept> depts = new ArrayList<IDept>();
+            for(Map<String,Object> map : list){
+                depts.add((Dept)DosolaUtil.convertMap(Dept.class,map));
+            }
+            return depts;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     @Override
